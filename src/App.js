9a243d12f90ec98b05dc853d22c0b72f6098c91d
mmonteleon <mmonteleon@mac.com>
2020-10-17 15:37:39 +0000

    super-collab maxwell & margie

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Card from './components/Card.js';
 import data from './atlas-of-remote-islands.js';
 
 function App() {
-  console.log(data);
   
+  let island1 = data.islands[0]
+  let island2 = data.islands[1]
+  let island3 = data.islands[2]
+  let island4 = data.islands[3]
+  console.log(data.islands[2])
+
   return (
     <div>
       <div className="container">
@@ -15,10 +20,18 @@ function App() {
       <div className="container">
         <h2 className="section-title">Islands</h2>
         <ul className="cards">
-          
-          <Card />
-          
-        </ul>
+{/*           
+          <Card name = {island1.name} image = {island1.image} description = {island1.description} coords ={island1.coords}/>
+          <Card name = {island2.name} image = {island2.image} description = {island2.description} coords ={island2.coords}/>
+          <Card name = {island3.name} image = {island3.image} description = {island3.description} coords ={island3.coords}/>
+          <Card name = {island4.name} image = {island4.image} description = {island4.description} coords ={island4.coords}/>
+           */}
+
+           {data.islands.map(island =>{
+               return (<Card island = {island} />)
+           })}
+
+           </ul>
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,13 @@ import '../App.css';
 import CardImage from './CardImage.js';
 import CardInfo from './CardInfo.js';
 
-function Card() {
+function Card(props) {
   return(
     <li className="cards-item fourth">
       <div className="card">
         <div className="card-content">
-            <CardImage />
-            <CardInfo />
+            <CardImage image = {props.island.image}/>
+            <CardInfo name= {props.island.name} description={props.island.description} coords ={props.island.coords}/>
         </div>
       </div>
     </li>
